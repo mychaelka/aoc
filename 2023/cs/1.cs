576460fb1013662fc976307c8ed9b1b4ab119fa2
Michaela Kecskésová <x460690@fi.muni.cz>
2023-12-01 22:21:44 +0100

    Day 1 solution

--- 2023/cs/1.cs
+++ 2023/cs/1.cs
@@ -1,47 +1,106 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
 
 public class CalibrationDocument {
-    private string inputPath;
-    
-    public CalibrationDocument(string inputPath) {
-        this.inputPath = inputPath;
-    }
 
-    public static int ReadInput(string filepath) {
-        int calibrationValue = 0;
-        char firstNum;
-        char lastNum;
-        string line = "";
-        StreamReader reader = new StreamReader(filepath)
-        while ((line = reader.ReadLine()) != null) {
-            for (int i = 0; i < line.Length; i++) {
-                char current = line[i];
-                
-                if (current >= 48 && current <= 57) {
-                    firstNum = current;
-                    break;
-                }
-            }
+    private static Dictionary<string, int> wordsToNums = new()
+    {
+        {"one", 1},
+        {"two", 2},
+        {"three", 3},
+        {"four", 4},
+        {"five", 5},
+        {"six", 6},
+        {"seven", 7},
+        {"eight", 8},
+        {"nine", 9}
+    };
 
-            for (int i = line.Length - 1; i >= 0; i--) {
-                char current = line[i];
-                
-                if (current >= 48 && current <= 57) {
-                    lastNum = current;
-                    break;
-                }
-            }
+    public static HashSet<int> FindSubstringIndices(string source, string substring)
+    {
+        HashSet<int> indices = new HashSet<int>();
+        if (String.IsNullOrEmpty(substring))
+        {
+            return indices;
         }
 
-        calibrationValue = 10 * (firstNum - 48) + (lastNum - 48);
+        int index = 0;
+        while ((index = source.IndexOf(substring, index, StringComparison.OrdinalIgnoreCase)) != -1)
+        {
+            indices.Add(index);
+            index += 1;
+        }
+
+        return indices;
+    }
+
+
+    public static int GetCalibrationValue(string filepath) {
+        int calibrationValue = 0;
+        int firstNum = 0;
+        int lastNum = 0;
+        String? line = "";
+        
+        using (StreamReader reader = new(filepath)) {
+            while ((line = reader.ReadLine()) != null) {
+
+                int substringFirstIndex = int.MaxValue;
+                int substringLastIndex = -1;
+                int min_idx = -1;
+                int max_idx = -1;
+
+                foreach (string key in wordsToNums.Keys) {
+                    
+                    HashSet<int> indices = FindSubstringIndices(line, key);
+                    if (indices.Count > 0) {
+                        min_idx = indices.Min();
+                        max_idx = indices.Max();
+                    }
+                    
+                    if (min_idx < substringFirstIndex && min_idx >= 0) {
+                        substringFirstIndex = min_idx;
+                        firstNum = wordsToNums[key];
+                    }
+                    
+                    if (max_idx > substringLastIndex && max_idx >= 0) {
+                        substringLastIndex = max_idx;
+                        lastNum = wordsToNums[key];
+                    }
+                }
+
+                for (int i = 0; i < line.Length; i++) {
+                    char current = line[i];
+                    
+                    if (Char.IsNumber(current)) {
+                        if (substringFirstIndex == -1 || (substringFirstIndex >= 0 && i < substringFirstIndex)) {
+                            firstNum = current - 48;
+                            break;
+                        }
+                    }
+                }
+
+                for (int i = line.Length - 1; i >= 0; i--) {
+                    char current = line[i];
+                    
+                    if (Char.IsNumber(current) && (i > substringLastIndex || substringLastIndex == -1)) {
+                        lastNum = current - 48;
+                        break;
+                    }
+                }
+                if (lastNum == 0 || firstNum == 0) {
+                    Console.WriteLine(line);
+                }
+                calibrationValue += 10 * firstNum + lastNum;
+            }
+        }
 
         return calibrationValue;
     
     }
 
     public static void Main() {
-        calibrationValue = this.ReadInput(inputPath);
+        int calibrationValue = GetCalibrationValue("../input1a.txt");
         Console.WriteLine(String.Format("Calibration value: {0}", calibrationValue));
     }
 }
